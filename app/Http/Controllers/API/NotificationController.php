26088c20ecbb25cd77a5e1489ae3ebc014d1f6e1
nitishk@instaacoders.com <nitishk@instaacoders.com>
2024-01-15 19:48:01 +0530

    process work to notification

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class NotificationController extends Controller
 {
     public function SendNotification()
     {
-        
+        // Notification::send()
+        // this is function will be used to send the notification 
     }
 }
